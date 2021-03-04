b7a58b3b09862089d50539d99d24ccbb94140359
Matthew Torres <matthew.torres211@gmail.com>
2021-03-05 00:19:34 +0800

    Add footer compoment

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Flex as='footer' borderTop='1px' borderColor='gray.200' h='65px'>
+      Footer
+    </Flex>
+  )
+}
+
+export default Footer
--- src/components/layouts/Protected.js
+++ src/components/layouts/Protected.js
@@ -1,7 +1,10 @@
 // import PropTypes from 'prop-types'
+import { Box } from '@chakra-ui/react'
 import React, { useHistory } from 'react-router-dom'
 import { useAuth } from '../../utils/hooks'
+import Footer from '../Footer'
 import Header from '../header/Header'
+import Hidden from '../Hidden'
 
 const Protected = ({ children }) => {
   const history = useHistory()
@@ -14,7 +17,12 @@ const Protected = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+      <Box as='main' h={{ base: 'calc(100vh - 130px)', lg: 'calc(100vh - 65px)' }}>
+        {children}
+      </Box>
+      <Hidden above='md'>
+        <Footer />
+      </Hidden>
     </>
   )
 }
