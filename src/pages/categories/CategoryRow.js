d25a67ad938c6b4987d0508e9286312a6709abc2
Matthew Torres <matthew.torres211@gmail.com>
2021-03-18 23:25:41 +0800

    Able to delete category

--- src/pages/categories/CategoryRow.js
+++ src/pages/categories/CategoryRow.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Tr, Td, useDisclosure } from '@chakra-ui/react'
-import { MdEdit } from 'react-icons/md'
+import { Tr, Td, useDisclosure, Box } from '@chakra-ui/react'
+import { MdEdit, MdDelete } from 'react-icons/md'
 import PropTypes from 'prop-types'
+import { useMutation, useQueryClient } from 'react-query'
 import IconButton from '../../components/IconButton'
 import ColorSwatch from '../../components/color-swatch/ColorSwatch'
 import ColorIcon from '../../components/color-swatch/ColorIcon'
+import { deleteCategory } from '../../services/categories'
 
 const CategoryRow = ({ data }) => {
+  const queryClient = useQueryClient
   const { isOpen: isHovered, onOpen: onMouseEnter, onClose: onMouseLeave } = useDisclosure()
+  const { mutate } = useMutation(deleteCategory, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('categories')
+    },
+  })
   // const { isOpen: isHovered } = useDisclosure()
   // const { isOpen: isEditing, onToggle } = useDisclosure()
   // console.log('isEditing', isEditing, data.name)
@@ -26,14 +34,25 @@ const CategoryRow = ({ data }) => {
       <Td>{data.name}</Td>
       <Td>
         {data.userId && (
-          <IconButton
-            opacity={isHovered ? '1' : '0'}
-            label='Edit Category'
-            ariaLabel='Edit category'
-            icon={<MdEdit />}
-            variant='ghost'
-            fontSize='20px'
-          />
+          <Box display='flex'>
+            <IconButton
+              opacity={isHovered ? '1' : '0'}
+              label='Edit Category'
+              ariaLabel='Edit category'
+              icon={<MdEdit />}
+              variant='ghost'
+              fontSize='20px'
+            />
+            <IconButton
+              opacity={isHovered ? '1' : '0'}
+              label='Delete Category'
+              ariaLabel='Delete category'
+              icon={<MdDelete />}
+              variant='ghost'
+              fontSize='20px'
+              onClick={() => mutate(data.id)}
+            />
+          </Box>
         )}
       </Td>
     </Tr>
--- src/services/categories.js
+++ src/services/categories.js
@@ -2,3 +2,4 @@ import fetch from '../utils/fetch'
 
 export const listCategories = () => fetch.get('/categories')
 export const createCategory = (data) => fetch.post('/categories', data)
+export const deleteCategory = (id) => fetch.delete(`/categories/${id}`)
