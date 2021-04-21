f81ba200eba16b54e28f5adb238b8f9e2c5ab6ad
Matthew Torres <matthew.torres211@gmail.com>
2021-04-21 20:23:31 +0800

    Create page for password reset

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,3 +3,4 @@ import fetch from '../utils/fetch'
 export const register = (data) => fetch.post('/users', data)
 export const login = (data) => fetch.post('/auth', { ...data, strategy: 'local' })
 export const logout = () => fetch.delete(`/auth`)
+export const forgotPassword = (data) => fetch.post('/forgot-password', data)
