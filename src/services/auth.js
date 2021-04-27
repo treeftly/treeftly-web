879a001948602fcdbfc3265089ec1eea86e150a6
Matthew Torres <matthew.torres211@gmail.com>
2021-04-27 23:16:58 +0800

    Add reset password functionality

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,4 +3,5 @@ import fetch from '../utils/fetch'
 export const register = (data) => fetch.post('/users', data)
 export const login = (data) => fetch.post('/auth', { ...data, strategy: 'local' })
 export const logout = () => fetch.delete(`/auth`)
+export const resetPassword = (token, data) => fetch.patch(`/forgot-password?token=${token}`, data)
 export const forgotPassword = (data) => fetch.post('/forgot-password', data)
