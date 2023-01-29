5cc4a5af3f9824af6c207f34453e26f7046ecbb6
Matthew Torres <matthew.torres211@gmail.com>
2023-01-29 13:58:38 +0800

    Update sorting

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -2,7 +2,7 @@ import fetch from '../utils/fetch'
 
 export const key = 'transactions'
 export const listTransactions = (query = '') =>
-  fetch.get(`/${key}?include=true&$sort[date]=1&${query}`)
+  fetch.get(`/${key}?include=true&$sort[date]=-1&${query}`)
 export const createTransaction = (data) => fetch.post(`/${key}`, data)
 export const getTransaction = (id) => fetch.get(`/${key}/${id}?include=true`)
 export const deleteTransaction = (id) => fetch.delete(`/${key}/${id}`)
